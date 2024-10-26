9534b3febd1f4fba5e4ae0cd313e50f7077e2f69
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2024-10-26 20:35:51 -0300

    se agregan validaciones a producto en rutas

--- routes/producto.js
+++ routes/producto.js
@@ -12,8 +12,64 @@ const router = Router();
 
 router.get('/', obtenerTodosProductos);
 router.get('/:id', obtenerUnProducto);
-router.post('/', crearProducto);
-router.put('/:id', editarProducto);
+router.post('/', [
+    check('nombre')
+    .notEmpty()
+    .withMessage('el campo nombre esta vacio')
+    .isLength({min:3 , max: 35 })
+    .withMessage(' el campo nombre debe tener entre 3 y 35 caracteres máximo'),
+    check('precio')
+    .notEmpty()
+    .withMessage('el campo precio esta vacío')
+    .isNumeric()
+    .withMessage('el campo precio debe ser del tipo numerico')
+    .custom((value) => {
+        if(value >= 1 && value <=1000000000){
+            return true
+        } else{
+            throw new Error('el precio debe estar entre 1 y 1000000000')
+        }
+    }),
+    check('descripcion')
+    .notEmpty()
+    .withMessage('el campo descripción esta vacio')
+    .isLength({min: 3, max:30})
+    .withMessage('el descripcion  debe tener como minimo 3 y maximo 30 caracteres'),
+    check('fecha')
+    .notEmpty()
+    .withMessage('el campo fecha esta vacio')
+    .isDate()
+    .withMessage(' debe ser del tipo fecha ')
+], crearProducto);
+router.put('/:id', [
+    check('nombre')
+    .notEmpty()
+    .withMessage('el campo nombre esta vacio')
+    .isLength({min:3 , max: 35 })
+    .withMessage(' el campo nombre debe tener entre 3 y 35 caracteres máximo'),
+    check('precio')
+    .notEmpty()
+    .withMessage('el campo precio esta vacío')
+    .isNumeric()
+    .withMessage('el campo precio debe ser del tipo numerico')
+    .custom((value) => {
+        if(value >= 1 && value <=1000000000){
+            return true
+        } else{
+            throw new Error('el precio debe estar entre 1 y 1000000000')
+        }
+    }),
+    check('descripcion')
+    .notEmpty()
+    .withMessage('el campo descripción esta vacio')
+    .isLength({min: 3, max:30})
+    .withMessage('el descripcion  debe tener como minimo 3 y maximo 30 caracteres'),
+    check('fecha')
+    .notEmpty()
+    .withMessage('el campo fecha esta vacio')
+    .isDate()
+    .withMessage(' debe ser del tipo fecha ')
+], editarProducto);
 router.delete('/:id', eliminarProducto);
 
 module.exports = router;
